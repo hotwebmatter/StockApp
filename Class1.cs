6853a9ecb0b08fac2df5d319e9705b5658eea698
Matt Obert on Windows <matt@hotwebmatter.com>
2019-10-06 21:48:19 -0400

    Initial commit of StockApp class and methods

---
+++ Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockApp
+{
+    public class StockAppCalculator
+    {
+        private string stockName;
+        private double stockSharesBought;
+        private double stockPriceBought;
+        private double stockPriceSold;
+        private const double COMMISSION = 0.03;
+        private double boughtPrice;
+        private double soldPrice;
+        private double difference;
+
+        // Default constructor for "Example, Inc." stock transaction
+        public StockAppCalculator()
+        {
+            stockName = "XMPL";
+            stockSharesBought = 200;
+            stockPriceBought = 25;
+            stockPriceSold = 30;
+        }
+
+        // Constructor with all data members passed as arguments to parameters
+        public StockAppCalculator(string name, double sharesBought, double priceBought, double priceSold)
+        {
+            stockName = name;
+            stockSharesBought = sharesBought;
+            stockPriceBought = priceBought;
+            stockPriceSold = priceSold;
+        }
+
+        //Properties
+        public string StockName
+        {
+            get
+            {
+                return stockName;
+            }
+            set
+            {
+                stockName = value;
+            }
+        }
+        public double StockSharesBought
+        {
+            get
+            {
+                return stockSharesBought;
+            }
+            set
+            {
+                stockSharesBought = value;
+            }
+        }
+
+        public double StockPriceBought
+        {
+            get
+            {
+                return stockPriceBought;
+            }
+            set
+            {
+                stockPriceBought = value;
+            }
+        }
+
+        public double StockPriceSold
+        {
+            get
+            {
+                return stockPriceSold;
+            }
+            set
+            {
+                stockPriceSold = value;
+            }
+        }
+        public double Buy()
+        {
+            boughtPrice = stockSharesBought * stockPriceBought * (1 + COMMISSION);
+            return boughtPrice;
+        }
+        public double Sell()
+        {
+            soldPrice = stockSharesBought * stockPriceSold * (1 - COMMISSION);
+            return soldPrice;
+        }
+        public double Difference()
+        {
+            difference = soldPrice - boughtPrice;
+            return difference;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * ######################################################
+ * ##    Programming Assignment #6                     ##
+ * ##    Developer: Matthew Obert                      ##
+ * ##    Date Submitted: October 06 2019               ##
+ * ##    Purpose: Demonstrate usage of OOP concepts.   ##
+ * ######################################################
+ */
+using System;
+using static System.Console;
+
+namespace StockApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockAppCalculator transaction = new StockAppCalculator();
+            string inputValue;
+            WriteLine("*******************************************************");
+            WriteLine("        Stock Transactions");
+            WriteLine("        Calculate results of stock bought and sold");
+            WriteLine("        Matthew Obert");
+            WriteLine("        October 06, 2019");
+            WriteLine("*******************************************************");
+            WriteLine("This program will determine profit or loss on stock.\n");
+            WriteLine("You will be asked to enter the stock name, number of shares,");
+            WriteLine(" price bought, and price sold.\n");
+            WriteLine("*******************************************************");
+            WriteLine("*******************************");
+            Write("Enter stock name: ");
+            WriteLine(transaction.StockName);
+            // inputValue = ReadLine();
+            // transaction.StockName.set(inputValue);
+            Write("Enter shares bought: ");
+            WriteLine(transaction.StockSharesBought);
+            // inputValue = ReadLine();
+            // transaction.SharesBought.set(double.Parse(inputValue));
+            Write("Enter price bought: ");
+            WriteLine(transaction.StockPriceBought);
+            // inputValue = ReadLine();
+            // transaction.PriceBought.set(double.Parse(inputValue));
+            Write("Enter price sold: ");
+            WriteLine(transaction.StockPriceSold);
+            // inputValue = ReadLine();
+            // transaction.PriceSold.set(double.Parse(inputValue));
+            WriteLine("*******************************");
+            Write("        ");
+            WriteLine(transaction.StockName);
+            Write("Bought price     ");
+            WriteLine("{0:C}", transaction.Buy());
+            Write("Sold price       ");
+            WriteLine("{0:C}", transaction.Sell());
+            Write("Difference       ");
+            WriteLine("{0:C}", transaction.Difference());
+            ReadLine();
+        }
+    }
+}
